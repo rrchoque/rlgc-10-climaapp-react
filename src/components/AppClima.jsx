6bda6e931a82357c9deb948896025b41be129e08
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-07-31 01:03:03 -0400

    Se agregaron los estilos del proyecto y el formulario

---
+++ src/components/AppClima.jsx
@@ -0,0 +1,15 @@
+import Formulario from "./Formulario"
+
+const AppClima = () => {
+
+  return (
+    <>
+        <main className="dos-columnas">
+            <Formulario />
+
+        </main>
+    </>
+  )
+}
+
+export default AppClima
---
+++ src/components/Formulario.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+
+const Formulario = () => {
+
+    return (
+        <div className="contenedor">
+            <form >
+                <div className="campo">
+                    <label htmlFor="ciudad">Ciudad</label>
+                    <input 
+                        type="text"
+                        id="ciudad"
+                        name="ciudad"
+                    />
+                </div>
+                <div className="campo">
+                    <label htmlFor="pais">País</label>
+                    <select
+                        id="pais"
+                        name="pais"
+                    >   
+                        <option value=""> Seleccione un país</option>
+                        <option value="US">Estados Unidos</option>
+                        <option value="MX">México</option>
+                        <option value="AR">Argentina</option>
+                        <option value="BO">Bolivia</option>
+                        <option value="CO">Colombia</option>
+                        <option value="CR">Costa Rica</option>
+                        <option value="ES">España</option>
+                        <option value="PE">Perú</option>
+                    </select>
+                </div>
+
+                <input
+                    type="submit"
+                    value='Consultar Clima'
+                />
+            </form>
+        </div>
+    )
+}
+
+export default Formulario
