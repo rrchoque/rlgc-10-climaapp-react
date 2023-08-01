270a5929bab91b404caced9ac4348cf1d5aefe4a
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2023-08-01 00:45:51 -0400

    Se concluyo el módulo de consulta a la api de clima

--- src/components/AppClima.jsx
+++ src/components/AppClima.jsx
@@ -1,12 +1,18 @@
+import useClima from "../hooks/useClima"
 import Formulario from "./Formulario"
+import Resultado from "./Resultado"
 
 const AppClima = () => {
+
+  const { resultado } = useClima()
 
   return (
     <>
         <main className="dos-columnas">
             <Formulario />
-
+            { 
+              resultado?.name && <Resultado />
+            }
         </main>
     </>
   )
